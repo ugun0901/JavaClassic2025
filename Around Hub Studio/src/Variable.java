497d20fdabc535037b74a3996e9d5d18aa331ae3
ugun0901 <ugun0901@sunmoon.ac.kr>
2024-12-28 16:17:50 +0900

    variable

---
+++ Around Hub Studio/src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        // 변수의 선언과 초기화
+        int a = 0;
+        double b = 2.0;
+        char c = 'a';
+        Boolean d = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}

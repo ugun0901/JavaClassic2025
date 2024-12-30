ad3990d197d395d80f41bf2ad3849c1c7d61aec6
ugun0901 <ugun0901@sunmoon.ac.kr>
2024-12-30 15:08:38 +0900

    practice

--- Around Hub Studio/src/Main.java
+++ Around Hub Studio/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        int d = a * b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }

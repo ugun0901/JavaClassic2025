72c59cd8d5b4eab2423158a283c28c5fdbad9fa0
ugun0901 <ugun0901@sunmoon.ac.kr>
2024-12-31 14:56:26 +0900

    last 2024

--- Around Hub Studio/src/Main.java
+++ Around Hub Studio/src/Main.java
@@ -5,10 +5,15 @@ public class Main {
         int b = 20;
         int c = a + b;
         int d = a * b;
+        int e = a / b;
+        int f = a % b;
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
     }
 }

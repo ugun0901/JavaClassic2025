a9d3da59bbe754d4f315a6b4e210ff4fc9589b08
ugun0901 <ugun0901@sunmoon.ac.kr>
2025-01-01 22:21:08 +0900

    Happy New Year

--- Around Hub Studio/src/Study.java
+++ Around Hub Studio/src/Study.java
@@ -1,5 +1,18 @@
 public class Study {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        int i;
+        int j;
+        int k;
+        int l;
+        int m;
+        int n;
+        int o;
+        int p;
+        int q;
+        int r;
+        int s;
     }
 }
